7b5a5c76cfe44db00be131ddd0144067c9b2dd39
Anoop B <16367697+anoop-b@users.noreply.github.com>
2024-11-18 15:35:03 +0200

    fix: env imports

--- src/routes/view/[url]/+server.ts
+++ src/routes/view/[url]/+server.ts
@@ -1,6 +1,6 @@
 import type { RequestHandler } from './$types';
 import { getS3SignedUrl } from '$lib/s3';
-import { PUBLIC_AWS_BUCKET_NAME } from '$env/static/public';
+import { AWS_BUCKET_NAME } from '$env/static/private';
 import { redirect } from '@sveltejs/kit';
 
 export const GET: RequestHandler = async ({ params }) => {
@@ -9,7 +9,7 @@ export const GET: RequestHandler = async ({ params }) => {
 	console.log("Returning presigned URL for:", url);
 
 	const downloadURL = await getS3SignedUrl({
-		bucket: PUBLIC_AWS_BUCKET_NAME,
+		bucket: AWS_BUCKET_NAME,
 		key: url
 	});
 	throw redirect(303, downloadURL);
